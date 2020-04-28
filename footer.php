b75c99a8e48b15d843ced81f4d0e58dd20dcf8ee
Afirehong <varhong2018@gmail.com>
2020-04-28 22:46:06 +0800

    修改小bug

--- footer.php
+++ footer.php
@@ -77,11 +77,11 @@
                 HNIE - Joey & AfireHong & Esson & Jonathan</p>
         </div>
     </footer>
-    <!-- <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
-    <script src="assets/js/smoothproducts.min.js"></script>
-    <script src="assets/js/theme.js"></script> -->
+    <script>
+        $(document).ready(function () {
+            
+        })
+    </script>
 </body>
 
 </html>
