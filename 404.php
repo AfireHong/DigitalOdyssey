d3f55bebf1cbaddfae913d3dfc549957dd683bd7
JonathanChuyan <1006781018a@gmail.com>
2020-04-30 11:04:11 +0800

    更改标题样式

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
         <section class="clean-block">
             <div class="container">
                 <div class="block-heading">
-                    <h2>来到了内容的荒芜之地。。。😢</h2>
+                    <h2 class="text-info">来到了内容的荒芜之地。。。😢</h2>
                 </div>
                 <div class="block-content">
                     <img src="assets/img/404.jpg" alt="404" width="100%">
