d9654ab1e09635930466de1b23fd2883e6684d38
JonathanChuyan <1006781018a@gmail.com>
2020-04-30 10:53:30 +0800

    完善404页面

--- 404.php
+++ 404.php
@@ -1,9 +1,18 @@
 <?php
     include 'header.php';
 ?>
-    <div>
-        <img scr='assets/img/404.jpg'>
-    </div>
+    <main class="page">
+        <section class="clean-block">
+            <div class="container">
+                <div class="block-heading">
+                    <h2>来到了内容的荒芜之地。。。😢</h2>
+                </div>
+                <div class="block-content">
+                    <img src="assets/img/404.jpg" alt="404" width="100%">
+                </div>
+            </div>
+        </section>
+    </main>
 <?php
     include 'footer.php';
 ?>
