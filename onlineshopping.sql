c364adb9087d4838530153d8abb53d657715e786
Afirehong <varhong2018@gmail.com>
2020-05-01 21:00:42 +0800

    修改部分数据表，添加收货地址表

--- onlineshopping.sql
+++ onlineshopping.sql
@@ -11,11 +11,29 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 28/04/2020 19:57:50
+ Date: 01/05/2020 20:58:35
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for address
+-- ----------------------------
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE `address`  (
+  `add_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `receiver` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `tel` int(11) NULL DEFAULT NULL,
+  `post` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`add_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of address
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for cate
@@ -36,20 +54,37 @@ CREATE TABLE `cate`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `goods`;
 CREATE TABLE `goods`  (
-  `goods_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `cate_id` int(10) NULL DEFAULT NULL,
-  `stock` int(11) NOT NULL,
-  `max_price` decimal(10, 2) NOT NULL,
-  `min_price` decimal(10, 2) NOT NULL,
-  `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `is_hot` tinyint(1) NULL DEFAULT NULL,
-  `is_sell` tinyint(1) NOT NULL,
+  `goods_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品id',
+  `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名',
+  `cate_id` int(10) NULL DEFAULT NULL COMMENT '分类id',
+  `stock` int(11) NOT NULL COMMENT '库存',
+  `is_hot` tinyint(1) NULL DEFAULT 0 COMMENT '是否热卖 0否 1是',
+  `max_price` decimal(10, 2) NOT NULL COMMENT '最高价格',
+  `min_price` decimal(10, 2) NOT NULL COMMENT '最低价格',
+  `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '描述',
+  `is_rec` tinyint(1) NULL DEFAULT NULL COMMENT '是否推荐 0否 1是',
+  `is_up` tinyint(1) NULL DEFAULT NULL COMMENT '是否上架 0否 1是',
+  `status` tinyint(1) NULL DEFAULT 0 COMMENT '是否放入回收站 0否 1是',
   PRIMARY KEY (`goods_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for img
+-- ----------------------------
+DROP TABLE IF EXISTS `img`;
+CREATE TABLE `img`  (
+  `img_id` int(255) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+  `goods_id` int(10) NULL DEFAULT NULL,
+  PRIMARY KEY (`img_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of img
 -- ----------------------------
 
 -- ----------------------------
@@ -77,22 +112,22 @@ CREATE TABLE `orders`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `password` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `tel` varchar(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `sex` tinyint(1) NULL DEFAULT NULL,
-  `age` int(3) NULL DEFAULT NULL,
-  `status` tinyint(1) NOT NULL,
-  `power` tinyint(1) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '用户名',
+  `password` varchar(32) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '密码',
+  `tel` varchar(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '电话',
+  `email` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `sex` tinyint(1) NULL DEFAULT 0 COMMENT '性别 0保密 1男 2女',
+  `age` int(3) NULL DEFAULT 0 COMMENT '年龄',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态 0为正常 1被禁',
+  `power` tinyint(1) NOT NULL DEFAULT 0 COMMENT '权力 0普通用户 1管理员',
   PRIMARY KEY (`user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 100003 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (100001, 'test', '202CB962AC59075B964B07152D234B70', '13012345678', '123@qq.com', 1, 1, 1, 4);
-INSERT INTO `users` VALUES (100002, 'teso', '202CB962AC59075B964B07152D234B70', '13012345670', '123', NULL, NULL, 1, 4);
+INSERT INTO `users` VALUES (100001, 'test', '202CB962AC59075B964B07152D234B70', '13012345678', '123@qq.com', 1, 1, 1, 0);
+INSERT INTO `users` VALUES (100002, 'teso', '202CB962AC59075B964B07152D234B70', '13012345670', '123', NULL, NULL, 1, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
