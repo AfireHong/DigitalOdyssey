f48c6e2f5180969681ad1566da745612bdd74d9f
Afirehong <varhong2018@gmail.com>
2020-05-07 10:46:10 +0800

    完善部分数据表结构

--- onlineshopping.sql
+++ onlineshopping.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 01/05/2020 20:58:35
+ Date: 07/05/2020 10:45:08
 */
 
 SET NAMES utf8mb4;
@@ -22,12 +22,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `address`;
 CREATE TABLE `address`  (
-  `add_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `address` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `receiver` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `tel` int(11) NULL DEFAULT NULL,
-  `post` int(11) NULL DEFAULT NULL,
+  `add_id` int(11) NOT NULL COMMENT '地址id',
+  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `address` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '地址',
+  `receiver` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '收件人',
+  `tel` varchar(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '电话',
+  `post` varchar(11) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '邮编',
   PRIMARY KEY (`add_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
@@ -40,8 +40,8 @@ CREATE TABLE `address`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `cate`;
 CREATE TABLE `cate`  (
-  `cate_id` int(11) NOT NULL,
-  `cate_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `cate_id` int(11) NOT NULL COMMENT '分类id',
+  `cate_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '分类名',
   PRIMARY KEY (`cate_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
@@ -57,9 +57,10 @@ CREATE TABLE `goods`  (
   `goods_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '商品id',
   `goods_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '商品名',
   `cate_id` int(10) NULL DEFAULT NULL COMMENT '分类id',
+  `brand` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '品牌',
   `stock` int(11) NOT NULL COMMENT '库存',
   `is_hot` tinyint(1) NULL DEFAULT 0 COMMENT '是否热卖 0否 1是',
-  `max_price` decimal(10, 2) NOT NULL COMMENT '最高价格',
+  `max_price` decimal(10, 2) NOT NULL COMMENT '最高价格,默认显示最高价格',
   `min_price` decimal(10, 2) NOT NULL COMMENT '最低价格',
   `description` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '描述',
   `is_rec` tinyint(1) NULL DEFAULT NULL COMMENT '是否推荐 0否 1是',
@@ -77,9 +78,9 @@ CREATE TABLE `goods`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `img`;
 CREATE TABLE `img`  (
-  `img_id` int(255) NOT NULL,
-  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
-  `goods_id` int(10) NULL DEFAULT NULL,
+  `img_id` int(255) NOT NULL COMMENT '图片id',
+  `img_url` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL COMMENT '图片名',
+  `goods_id` int(10) NULL DEFAULT NULL COMMENT '商品id',
   PRIMARY KEY (`img_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
@@ -92,14 +93,15 @@ CREATE TABLE `img`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders`  (
-  `orders_id` int(10) NOT NULL,
-  `goods_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `goods_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `is_pay` tinyint(1) NOT NULL,
-  `is_send` tinyint(1) NOT NULL,
-  `is_receive` tinyint(1) NOT NULL,
-  `is_status` tinyint(1) NOT NULL,
+  `orders_id` int(10) NOT NULL COMMENT '订单Id',
+  `orders_price` decimal(10, 2) NOT NULL COMMENT '订单总金额',
+  `goods_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '产品id，有多个逗号分隔',
+  `goods_count` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL COMMENT '产品数量，有多个逗号分隔',
+  `user_id` int(11) NOT NULL COMMENT '用户Id',
+  `is_pay` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否已支付，0未，1已，下面同',
+  `is_send` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否发送',
+  `is_receive` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否收到',
+  `is_status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否放入回收站',
   PRIMARY KEY (`orders_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
