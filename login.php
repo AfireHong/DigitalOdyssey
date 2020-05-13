1f39d81e3a72a4c5c481cb85bd4e15468f848fc4
Jonathan <1006781018a@gmail.com>
2020-05-13 11:14:48 +0800

    recaptcha自适应

--- login.php
+++ login.php
@@ -97,25 +97,22 @@
             })
         });
         $(function(){
-  function rescaleCaptcha(){
-    var width = $('.g-recaptcha').parent().width();
-    var scale;
-    if (width < 302) {
-      scale = width / 302;
-    } else{
-      scale = 1.0; 
-    }
-
-    $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
-    $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
-    $('.g-recaptcha').css('transform-origin', '0 0');
-    $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
-  }
-
-  rescaleCaptcha();
-  $( window ).resize(function() { rescaleCaptcha(); });
-
-});
+            function rescaleCaptcha(){
+                var width = $('.g-recaptcha').parent().width();
+                var scale;
+                if (width < 302) {
+                    scale = width / 302;
+                } else{
+                    scale = 1.0; 
+                }
+                $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('transform-origin', '0 0');
+                $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+            }
+            rescaleCaptcha();
+            $( window ).resize(function() { rescaleCaptcha(); });
+        });
     </script>
     <script src='https://www.recaptcha.net/recaptcha/api.js?hl=zh-CN'></script>
 <?php
